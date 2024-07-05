8313c2087f0e5ff25cc0b1db93d9fb1f2e4d6003
Martin Jönsson <martin.t.joensson@gmail.com>
2024-07-06 00:14:23 +0200

    Started on container component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Header } from "./components/Sections/Header";
 import playerTemplate from "./components/data/characterTemplate.json";
 import { Footer } from "./components/Sections/Footer";
 import { CeruleaRoutes } from "./routes/CeruleaRoutes";
+import { Container } from "./components/Container";
 
 export const App = () => {
   const [hero, setHero] = useState({ playerTemplate });
@@ -15,6 +16,7 @@ export const App = () => {
   return (
     <>
       <Header />
+      <Container />
       <CeruleaRoutes
         hero={hero}
         setHero={setHero}
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,26 @@
-
+import heart from "/images/heart-solid.svg";
+import bolt from "/images/bolt-lightning-solid.svg";
 
 export const Container = (type, filled) => {
+  console.log("Container type:", type);
 
-    console.log("Container type:", type);
+  console.log("Containers to be filled:", filled);
 
-    console.log("Containers to be filled:", filled);
+  const containerLength = 12;
 
-    const containerLength = 12;
+  const iconClass = "w-2 h-2";
+
+    const populate = (icon) => {
+        for (let runs = 0; runs < containerLength; runs++){
+          <img className={iconClass} src={icon} />
+        }
+    }
   return (
-    <div>Container</div>
-  )
-}
+    <div className="flex">
+          Container
+        {populate(heart)}
+      <img className={iconClass} src={heart} />
+      <img className={iconClass} src={bolt} />
+    </div>
+  );
+};
