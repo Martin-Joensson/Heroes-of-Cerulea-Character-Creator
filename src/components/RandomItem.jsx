30722102fa768357cc9ebf525a333b0f79739fab
Martin Jönsson <martin.t.joensson@gmail.com>
2024-07-12 00:32:49 +0200

    Added random items to shop

---
+++ src/components/RandomItem.jsx
@@ -0,0 +1,37 @@
+export const RandomItem = ({ numberOfItems, itemList }) => {
+  console.log("nr Items: ",numberOfItems, "List: ",itemList)
+
+  const selectedItems = [];
+
+  const RandomNumber = () => {
+    return Math.floor(Math.random() * itemList.length);
+  };
+
+  const SelectItems = () => {
+    while (selectedItems.length < numberOfItems) {
+      console.log("Selected items length:", selectedItems.length)
+      let item = itemList[RandomNumber()];
+
+      if (!selectedItems.includes(item)) {
+        selectedItems.push(item);
+      }
+    }
+  }
+
+  SelectItems();
+
+  console.log("Selected items: ",selectedItems)
+
+  return (
+    <div>
+      Random item{" "}
+      {selectedItems.map((item, index) => (
+        <div className="border p-2 m-2" key={index}>
+          <p className="font-enterCommandBold"> {item.name}</p>
+          <p className="leading-none">{item.description}</p>
+          <p>{item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,19 +1,35 @@
-import { GeneralItem } from "../components/GeneralItem";
+import { RandomItem } from "../components/RandomItem";
 import { useState } from "react";
+import shopItems from "../components/data/shopItems.json";
+import generalItems from "../components/data/generalItems.json";
 
 export const Shop = () => {
-const [inStock, setInStock] = useState(0)
+  const [inStock, setInStock] = useState(3);
 
   const howManyItemsInStock = () => {
     const inStockVar = 4;
-    setInStock( Math.floor(Math.random() * inStockVar + 1 + 1));
+    setInStock(Math.floor(Math.random() * inStockVar + 1 + 1));
   };
 
   return (
     <div className="">
       <p>Shop - Not ready yet</p>
-          <button onClick={howManyItemsInStock}>In Stock? {inStock} items</button>
-          <GeneralItem inStock={ inStock} />
+      <button onClick={howManyItemsInStock}>In Stock? {inStock} items</button>
+      <RandomItem numberOfItems={inStock} itemList={shopItems.shopItems} />
+      {/* {generalItems.generalItems.map((item, index) => (
+        <div className="border p-2 m-2" key={index}>
+          <p className="font-enterCommandBold"> {item.name}</p>
+          <p className="leading-none">{item.description}</p>
+          <p>{item.price}</p>
+        </div>
+      ))}
+      {shopItems.shopItems.map((item, index) => (
+        <div className="border p-2 m-2" key={index}>
+          <p className="font-enterCommandBold"> {item.name}</p>
+          <p className="leading-none">{item.description}</p>
+          <p>{item.price}</p>
+        </div>
+      ))} */}
     </div>
   );
 };
